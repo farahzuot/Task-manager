6d6d14bfee193cb73d8a54fd5cfe497c90b804cd
farah <farahalzuot97@gmail.com>
2025-02-01 23:23:12 +0300

    create view, url and template for tasks list page

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from tasks.views import TaskViewSet
-
+from tasks.views import TaskViewSet, TaskListView, TaskCreateView
 
 app_name = 'tasks'
 router = DefaultRouter()
@@ -10,4 +9,7 @@ router.register(r'api/tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('tasks/', TaskListView.as_view(), name='tasks-list'),
+    path('tasks/create/', TaskCreateView.as_view(), name='add-task'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
 from rest_framework import generics, permissions, viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -26,5 +28,17 @@ class TaskViewSet(viewsets.ModelViewSet):
 
 
 
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
 
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
 
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = 'tasks/task_create.html'
+    context_object_name = 'task'
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
