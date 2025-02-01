388406ef2a46a960618d126f98b00beb984cff98
farah <farahalzuot97@gmail.com>
2025-02-02 02:31:36 +0300

    create new task, task details pages and update the tasks list

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -6,7 +6,7 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         read_only_fields = ('id','user','updated_at', 'created_at' )
-        fields = ['id','user','title', 'description', 'updated_at', 'created_at']
+        fields = ['id','user','title', 'description', 'updated_at', 'created_at', 'completed']
 
     def create(self, validated_data):
         # Automatically set the user to the authenticated user
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, CreateView, DetailView
 from rest_framework import generics, permissions, viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -27,7 +30,6 @@ class TaskViewSet(viewsets.ModelViewSet):
         return Task.objects.filter(user=self.request.user)
 
 
-
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'tasks/task_list.html'
@@ -36,9 +38,36 @@ class TaskListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
 
+
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     template_name = 'tasks/task_create.html'
+    fields = ['title', 'description', 'due_date']
+    success_url = reverse_lazy('tasks:tasks-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
     context_object_name = 'task'
+
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+
+    def get_object(self):
+        # Ensures the task ID is used to retrieve the correct task
+        task_id = self.kwargs['id']
+        return Task.objects.get(id=task_id, user=self.request.user)
+
+
+class TaskUpdateStatusView(View):
+    def patch(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=kwargs['pk'], user=request.user)
+        task.completed = not task.completed
+        task.save()
+
+        return JsonResponse({'completed': task.completed})
